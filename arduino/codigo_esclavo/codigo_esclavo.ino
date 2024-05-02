47ed23723f0138338a32239be87a8788965453cf
Juan <josemargri3@gmail.com>
2024-05-02 08:30:57 -0500

    refactor : Se modulariza el proceso de inicialización de la consola y la comunicación inicial del Arduino como esclavo de Python

--- arduino/codigo_esclavo/codigo_esclavo.ino
+++ arduino/codigo_esclavo/codigo_esclavo.ino
@@ -2,28 +2,19 @@ const int BAUD_RATE = 9600;
 
 // variables globales
 String buffer = ""; // almacena los caracteres recibidos desde el puerto serial, hasta conformar el mensaje
-String message_from_serial = "x"; // almacena el mensaje recibido desde el puerto serial
+String message_from_serial = ""; // almacena el mensaje recibido desde el puerto serial
 String message_to_serial = "";   // almacena el mensaje a ser enviado hacia el puerto serial
 bool new_message_from_serial = false;    // true si se ha recibido un nuevo mensaje completo
+bool status = "OFF";
 
 void setup() {
   Serial.begin(BAUD_RATE);
   delay(100);
-  while(Serial.available()){
-    char inchar = Serial.read();
-  }
-  Serial.println("Arduino ... OK");
+  initialize();
 }
 
 void loop() {
-    serialEvent();
-    if(new_message_from_serial){
-      if(message_from_serial == "Python ... OK"){
-        Serial.println("Esperando Respuesta");
-        Serial.flush();
-      }
-      new_message_from_serial = false;
-    } 
+  serialEvent();
 }
 
 void serialEvent() {
@@ -43,4 +34,22 @@ void serialEvent() {
     }
   }
 } 
+
+void initialize(){
+  while(Serial.available()){
+    char inchar = Serial.read();
+  }
+  Serial.println("Arduino ... OK");
+  while(true){
+    serialEvent();
+    if(new_message_from_serial){
+      if(message_from_serial == "Python ... OK"){
+        Serial.println("Esperando Comando");
+        Serial.flush();
+      }
+      new_message_from_serial = false;
+      break;
+    }
+  } 
+}
 
